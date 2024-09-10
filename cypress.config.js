37c344b7b666499bd9034e45dd2efe0dc2ef9e1e
Mailton Carvalho <mailton.fenix@gmail.com>
2024-09-10 15:24:14 -0300

    insert conf cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
+    baseUrl: "https://phptravels.net/admin/login.php",
+    specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("file:preprocessor",cucumber());
     },
   },
 });
